5585b688fd8124113cfc7d2aaf052e95c91abbcc
cona-pixel <cons.castroo@duocuc.cl>
2026-06-01 21:46:22 -0400

    Primer codigo en Python

---
+++ 1_inventario.py/calculadora.py
@@ -0,0 +1,12 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+print("Calculadora simple")
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+
+print("Suma:", sumar(num1, num2))
+print("Resta:", restar(num1, num2))
